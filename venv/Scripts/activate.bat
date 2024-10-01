fa332d931d5d66b332856f864879750a927de1d8
Terence Jiang <terencejiang2004@gmail.com>
2024-10-01 14:22:18 -0400

    changes made to run venv on my system

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\kavin\OneDrive\Documents\Group-40-CH\venv
+set VIRTUAL_ENV=C:\Users\teren\OneDrive - Queen's University\Year Three\CISC 327\Group-40-CH\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
