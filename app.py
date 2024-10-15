e40e28ac0ba0bcc5c697ad7d089ed3e23f2da7f7
21KLA5 <111718383+21KLA5@users.noreply.github.com>
2024-10-15 18:38:52 -0400

    Basic registration functionality: check password, email, and missing fields

--- app.py
+++ app.py
@@ -1,13 +1,41 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+#mock database
+database = {}
+
+
 @app.route('/', methods=['GET', 'POST'])
-def index():
+def register():
     if request.method == 'POST':
-        user_input = request.form['user_input']
-        return render_template('index.html', user_input=user_input)
-    return render_template('index.html', user_input=None)
+        # Get form data
+        firstName = request.form['firstName']
+        lastName = request.form['lastName']
+        email = request.form['email']
+        password = request.form['password']
+        confirmPass = request.form['confirmPass']
+
+        #Check if passwords match
+        if password != confirmPass:
+            return "passwords do not match"
+
+        # Check if the username already exists in the database
+        if email in database:
+            return "email already registered"
+        
+        if not firstName or not lastName or not email or not password or not confirmPass:
+            return "missing required field"
+        
+        # Store user details in the mock database
+        database[email] = {'firstName': firstName, 'lastName': lastName, 'password': password}
+        
+        # Redirect to login page (Terence put url here)
+        return redirect(url_for(email=email))
+    
+    # For GET requests, display the registration form
+    return render_template('register.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
