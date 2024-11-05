92c31c6051d0bb22cd4c5a8333226905e7c0e69c
21KLA5 <111718383+21KLA5@users.noreply.github.com>
2024-11-05 15:54:36 -0500

    Change homepage to flightSearch, Access log in and register from homepage, access profile management if user is logged in.

--- app.py
+++ app.py
@@ -15,12 +15,13 @@ db = client["user_database"]
 users_collection = db["users"]
 bookings_collection = db["bookings"]
 
-@app.route('/index', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        user_input = request.form['user_input']
-        return render_template('index.html', user_input=user_input)
-    return render_template('index.html', user_input=None)
+@app.route('/', methods=['GET', 'POST'])
+def flightSearch():
+    user = None
+    if session:
+        email = session['user_email']
+        user = users_collection.find_one({"email": email})
+    return render_template('flightSearch.html', user=user)
 
 
 # Profile Management route
@@ -51,11 +52,11 @@ def profile(email):
                 users_collection.update_one({"email": email}, {"$set": update_fields})
                 return redirect(url_for('profile', email=new_email))
         elif action == 'Discard Changes':
-            return redirect(url_for('index'))
+            return redirect(url_for('flightSearch'))
     return render_template('profile.html', user=user, email=email, error_message=error_message)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
 
@@ -107,7 +108,7 @@ def login():
         else:
             if db_user['password'] == user_password:
                 session['user_email'] = user_email
-                return redirect(url_for('seat_selection'))
+                return redirect(url_for('flightSearch'))
             else:
                 return render_template('login.html', errorMessage="Email and password does not match")
     return render_template('login.html')
